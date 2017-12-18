21eb9f00cc6770caa0475fd960b322862c71e5b0
Kerzana <33467099+Kerzana@users.noreply.github.com>
2017-12-18 14:42:02 +0100

    Update GateAdresseDialInGate.lua

--- GateAdresseDialInGate.lua
+++ GateAdresseDialInGate.lua
@@ -8,8 +8,8 @@ function start()
 		if state[0] ~= Offline then
 			p.disconnect()
 		end
-		if p.dial(adresse) == true then
-			print(adresse)
+		if p.dial(adresse.."-00") == true then
+			print(adresse.."-00")
 		end
 		sleep(0)
 	end
